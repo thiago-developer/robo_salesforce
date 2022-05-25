751ae613106b04b1ab17dbb0ce51daa6cf380f2a
thiago-developer <thiago.gomes.developer@gmail.com>
2022-05-25 00:16:30 -0300

    implementação do metodo filtra id

--- robo_salesforce.py
+++ robo_salesforce.py
@@ -12,20 +12,25 @@ class RoboSalesforce:
 
     # metodo contrutor que recebe por parâmetro o objeto a ser realizado envio dos anexos.
     def __init__(self, objeto, ambiente, usuario, senha):
-        self.objeto = objeto
-        self.ambiente = ambiente
-        self.usuario = usuario
-        self.senha = senha
+        self._objeto = objeto
+        self._ambiente = ambiente
+        self._usuario = usuario
+        self._senha = senha
 
     def abre_navegador(self):
         pass
 
-    def ambiente_de_acesso_workbench(self, ambiente):
+    def _ambiente_de_acesso_workbench(self, ambiente):
         dev_homol = ambiente.upper() == "desenvolvimento".upper() or ambiente.upper() == "homologacao".upper()
         if dev_homol:
             return "Sandbox"
         if not dev_homol:
             return "Production"
+
+    def _filtra_id_objeto(self, href):
+        posicao_encontrada = href.find('=')
+        id_objeto = href[posicao_encontrada + 1:]
+        return id_objeto
 
     # metodo que realiza a query para pegar os id's a ser enviado
     def lista_ids_objeto(self):
@@ -38,22 +43,27 @@ class RoboSalesforce:
         driver = webdriver.Chrome(options=options, service=s)
         driver.get("https://workbench.developerforce.com/login.php")
         # configuracao sandbox
-        driver.find_element(By.XPATH, value='//*[@id="oauth_env"]').send_keys(self.ambiente_de_acesso_workbench(self.ambiente))
+        driver.find_element(By.XPATH, value='//*[@id="oauth_env"]').send_keys(self._ambiente_de_acesso_workbench(self._ambiente))
         driver.find_element(By.XPATH, value='//*[@id="termsAccepted"]').click()
         driver.find_element(By.XPATH, value='//*[@id="loginBtn"]').click()
-        driver.find_element(By.XPATH, value='//*[@id="username"]').send_keys(self.usuario)
-        driver.find_element(By.XPATH, value='//*[@id="password"]').send_keys(self.senha)
+        driver.find_element(By.XPATH, value='//*[@id="username"]').send_keys(self._usuario)
+        driver.find_element(By.XPATH, value='//*[@id="password"]').send_keys(self._senha)
         driver.find_element(By.XPATH, value='//*[@id="Login"]').click()
         driver.find_element(By.XPATH, value='//*[@id="actionJump"]').send_keys("SOQL Query")
         driver.find_element(By.XPATH, value='//*[@id="mainBlock"]/form/input[2]').click()
         driver.find_element(By.XPATH, value='//*[@id="soql_query_textarea"]').send_keys(
                                                                                         f"SELECT Id, Id_legado__c "
-                                                                                        f"FROM {self.objeto} "
+                                                                                        f"FROM {self._objeto} "
                                                                                         f"WHERE Id_legado__c !=null "
                                                                                         f"AND AnexoLegado__c = null "
                                                                                         f"Order By Id DESC")
         driver.find_element(By.XPATH, value='//*[@id="query_form"]/table/tbody/tr[3]/td[1]/input').click()
+        time.sleep(5)
+        id_institucional = self._filtra_id_objeto(driver.find_element(By.XPATH, '//*[@id="query_results"]/tbody/tr[2]/td[2]/a').get_property("href"))
+        id_legado = self._filtra_id_objeto(driver.find_element(By.XPATH, '//*[@id="query_results"]/tbody/tr[2]/td[3]/a').get_property("href"))
+        print(f"Id institucional: {id_institucional}, \nId IU Conecta: {id_legado}")
 
 
-robo = RoboSalesforce("WV_Suspensao__c", "producao", "thiago.a.santos-gomes@institucionais.com.br", "Thi12071995")
+
+robo = RoboSalesforce("objeto", "ambiente", "usuario", "senha")
 robo.lista_ids_objeto()
